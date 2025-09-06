746cba8cce4d2e4c278e9829d74f513417fd6d75
Quézia Marques Mendes <quezia.mendes@a.ucb.br>
2025-09-05 21:57:27 -0300

    Update Tarefa.java

---
+++ src/model/Tarefa.java
@@ -0,0 +1,70 @@
+import java.time.LocalDateTime;
+
+
+
+public class Tarefa {
+    private long id;
+    private String titulo;
+    private String descricao;
+    private LocalDateTime  dataHora;
+    private boolean  completo;
+    
+    //Costrutores
+    public Tarefa(){
+        this.dataHora=LocalDateTime.now();
+        this.completo=false;
+    }
+    
+    public Tarefa(String titulo){
+    this();    //chama o construtor padrão 
+    this.titulo=titulo;
+    }
+    
+    public Tarefa(String titulo,String descricao){
+    this(titulo);
+    this.descricao=descricao;
+    }
+    
+   
+    //gets e sets METODOS
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public boolean isComplemento() {
+        return completo;
+    }
+
+    public void setComplemento(boolean complemento) {
+        this.completo = complemento;
+    }
+  
+}
